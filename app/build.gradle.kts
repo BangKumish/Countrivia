116e81d79c5e65da39593c1a1ddffc793841b275
BangKumis <asihotang123@gmail.com>
2023-12-14 20:11:12 +0700

    Sixth Commit, Registration and Login with Room Database and OnBoarding Motion Layout //issue: frontend layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,6 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -46,11 +43,11 @@ android {
         dataBinding = true
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+    composeOptions{
+        kotlinCompilerExtensionVersion = "1.5.0"
     }
-    packaging {
-        resources {
+    packaging{
+        resources{
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
@@ -69,14 +66,10 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
 
     //Compose
     implementation("androidx.activity:activity-compose:1.8.1")
@@ -84,14 +77,12 @@ dependencies {
     implementation ("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation ("androidx.compose.material:material:1.5.4")
     implementation("io.coil-kt:coil-compose:2.2.2")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation ("androidx.compose.ui:ui-tooling:1.5.4")
     debugImplementation ("androidx.compose.ui:ui-test-manifest:1.5.4")
 
     //Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     //Preference DataStore
     implementation("androidx.datastore:datastore-preferences-android:1.1.0-alpha07")
@@ -125,4 +116,10 @@ dependencies {
     //noinspection KaptUsageInsteadOfKsp
     kapt ("com.github.bumptech.glide:compiler:4.13.0")
     implementation ("com.github.2coffees1team:GlideToVectorYou:v2.0.0")
+
+    //viewPager2 Library
+    implementation ("androidx.viewpager2:viewpager2:1.1.0-beta02")
+
+    //stepper Library
+    implementation ("com.tbuonomo:dotsindicator:5.0")
 }
---
+++ app/src/main/java/id/finale/countrivia/onBoarding/customView/OnBoardingView.kt
@@ -0,0 +1,97 @@
+package id.finale.countrivia.onBoarding.customView
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.viewpager2.widget.ViewPager2
+import id.finale.countrivia.activity.LoginActivity
+import id.finale.countrivia.databinding.OnboardingViewBinding
+import id.finale.countrivia.onBoarding.OnBoardingPagerAdapter
+import id.finale.countrivia.onBoarding.entity.OnBoardingPage
+import id.finale.countrivia.onBoarding.onBoardingHelper.core.setParallaxTransformation
+import id.finale.countrivia.onBoarding.onBoardingHelper.domain.OnBoardingPrefManager
+
+class OnBoardingView @JvmOverloads
+constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+): FrameLayout (context, attrs, defStyleAttr, defStyleRes){
+    private val numberOfPages by lazy { OnBoardingPage.values().size }
+    private val prefManager: OnBoardingPrefManager
+
+
+    init {
+        val binding = OnboardingViewBinding.inflate(LayoutInflater.from(context), this, true)
+        with(binding) {
+            setUpSlider()
+            addingButtonsClickListeners()
+            prefManager = OnBoardingPrefManager(root.context)
+        }
+
+    }
+
+    private fun OnboardingViewBinding.setUpSlider() {
+        with(slider) {
+            adapter = OnBoardingPagerAdapter()
+
+            setPageTransformer { page, position ->
+                setParallaxTransformation(page, position)
+            }
+//
+//            setPageTransformer(pageCompositePageTransformer)
+
+            addSlideChangeListener()
+
+            val wormDotsIndicator = pageIndicator
+            wormDotsIndicator.attachTo(this)
+        }
+    }
+
+
+    private fun OnboardingViewBinding.addSlideChangeListener() {
+
+        slider.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                if (numberOfPages > 1) {
+                    val newProgress = (position + positionOffset) / (numberOfPages - 1)
+                    onboardingRoot.progress = newProgress
+                }
+            }
+        })
+    }
+
+    private fun OnboardingViewBinding.addingButtonsClickListeners() {
+        nextBtn.setOnClickListener { navigateToNextSlide(slider) }
+        skipBtn.setOnClickListener {
+            setFirstTimeLaunchToFalse()
+            goToLogin(context)
+        }
+        startBtn.setOnClickListener {
+            setFirstTimeLaunchToFalse()
+            goToLogin(context)
+        }
+    }
+
+    private fun setFirstTimeLaunchToFalse() {
+        prefManager.isFirstTimeLaunch = false
+    }
+
+    private fun navigateToNextSlide(slider: ViewPager2?) {
+        val nextSlidePos: Int = slider?.currentItem?.plus(1) ?: 0
+        slider?.setCurrentItem(nextSlidePos, true)
+    }
+
+    private fun goToLogin(context: Context){
+        val intent = Intent(context, LoginActivity::class.java)
+        context.startActivity(intent)
+    }
+}
